46c279fb5bab0bce836b52ff10cfc572abe79d7f
Thomas Marlow <43114877+thomasmarlow@users.noreply.github.com>
2019-10-22 00:50:41 -0300

    Delete migracion_carga_rubros.sql

--- migracion_carga_rubros.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROC migracion_carga_rubros AS
-BEGIN
-	INSERT INTO Rubro (
-		rubro_descripcion
-	)
-	SELECT DISTINCT Provee_Rubro
-	FROM gd_esquema.Maestra
-	WHERE Provee_Rubro IS NOT NULL;
-END

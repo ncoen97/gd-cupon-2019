c32ff881654beb77ea0db74b3967d946c8e76876
frodriguezsandez <frodriguezsandez@hotmail.com>
2019-11-05 17:32:32 -0300

    forms con inputs

--- Aplicacion/FrbaOfertas/FrbaOfertas/AbmRol/Form1.Designer.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/AbmRol/Form1.Designer.cs
@@ -28,11 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(250, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 26);
+            this.textBox2.TabIndex = 43;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Rol";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(220, 65);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(102, 20);
+            this.label.TabIndex = 46;
+            this.label.Text = "Registrar Rol";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 40);
+            this.button1.TabIndex = 47;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 388);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button1;
     }
 }

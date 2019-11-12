a15b2cd307097a7783aecc06077d51f0b0ffb8f6
Juan Mesaglio <juan.mesaglio@gmail.com>
2019-11-12 17:04:06 -0300

    la wae de c#

--- Aplicacion/FrbaOfertas/FrbaOfertas/DAOs/UsuarioDAO.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/DAOs/UsuarioDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace FrbaOfertas.DAOs
 {
@@ -12,12 +13,19 @@ namespace FrbaOfertas.DAOs
         public static int logUsuario(Usuario usuario)
         {
             SqlConnection conexion = DBConnection.getConnection();
-            SqlCommand command = new SqlCommand("", conexion);
-            //aca se ejecuta una stored procedure de logear usuario y devuelve el id
+            SqlCommand command = new SqlCommand("SOCORRO.validarLogin", conexion);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@username", usuario.username);
+            command.Parameters.AddWithValue("@password", usuario.password);
+            SqlParameter ret = new SqlParameter();
+            ret.Direction = ParameterDirection.ReturnValue;
+            command.Parameters.Add(ret);
+            command.ExecuteReader();
             command.Dispose();
             conexion.Close();
             conexion.Dispose();
-            return 1;//devuelve el id de usuario
+            return (int)ret.Value;
         }
+
     }
 }

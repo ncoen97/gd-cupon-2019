281905d3461f331da3baa4868c25ccd40d03c9e9
frodriguezsandez <frodriguezsandez@hotmail.com>
2019-12-01 18:29:50 -0300

    anda hab/deshab rol

--- Aplicacion/FrbaOfertas/FrbaOfertas/AbmRol/AbmRol.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/AbmRol/AbmRol.cs
@@ -15,6 +15,7 @@ namespace FrbaOfertas
     public partial class AbmRol : Form
     {
         Usuario usuario;
+        DataGridViewRow selectedRow = null;
         public AbmRol(Usuario _usuario)
         {
             InitializeComponent();
@@ -23,14 +24,15 @@ namespace FrbaOfertas
 
         private void Button1_Click(object sender, EventArgs e)
         {
-
+            Rol r = new Rol(rol_nombre.Text, null);
+            DBConnection.registrar_Rol(r);
         }
 
         private void AbmRol_Load(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
             SqlConnection conexion = DBConnection.getConnection();
-            SqlCommand command = new SqlCommand("select rol_nombre, func_descripcion from SOCORRO.Rol r join(SOCORRO.FuncionalidadxRol fxr join SOCORRO.Funcionalidad f on fxr.func_id = f.func_id) on r.rol_id = fxr.rol_id where rol_habilitado=1 order by rol_nombre", conexion);
+            SqlCommand command = new SqlCommand("select rol_habilitado, r.rol_id, rol_nombre, func_descripcion from SOCORRO.Rol r join(SOCORRO.FuncionalidadxRol fxr join SOCORRO.Funcionalidad f on fxr.func_id = f.func_id) on r.rol_id = fxr.rol_id order by rol_nombre", conexion);
             command.CommandType = CommandType.Text;
 
             SqlDataAdapter da = new SqlDataAdapter(command);
@@ -39,11 +41,92 @@ namespace FrbaOfertas
             dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
         }
 
+        private void actualizar()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conexion = DBConnection.getConnection();
+            SqlCommand command = new SqlCommand("select rol_habilitado, r.rol_id, rol_nombre, func_descripcion from SOCORRO.Rol r join(SOCORRO.FuncionalidadxRol fxr join SOCORRO.Funcionalidad f on fxr.func_id = f.func_id) on r.rol_id = fxr.rol_id order by rol_nombre", conexion);
+            command.CommandType = CommandType.Text;
+
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+
+        }
+
         private void button5_Click(object sender, EventArgs e)
         {
             MenuAdministrador oc = new MenuAdministrador(usuario);
             oc.Show();
             this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (selectedRow == null)
+                return;
+
+            const string message =
+            "Esta por habilitar este rol. Es lo que quiere hacer?";
+            const string caption = "Habilitar rol";
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Rol rol = new Rol(
+                (int)selectedRow.Cells[1].Value,
+                selectedRow.Cells[2].Value.ToString(),
+                (bool)selectedRow.Cells[0].Value
+                );
+
+                DBConnection.rehabilitar_rol(rol);
+                actualizar();
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (selectedRow == null)
+                return;
+
+            const string message =
+            "Esta por deshabilitar este rol. Es lo que quiere hacer?";
+            const string caption = "Deshabilitar rol";
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+             
+                Rol rol = new Rol(
+                (int)selectedRow.Cells[1].Value,
+                selectedRow.Cells[2].Value.ToString(),
+                (bool)selectedRow.Cells[0].Value
+                );
+
+                DBConnection.deshabilitar_rol(rol);
+                actualizar();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            if (index >= 0)
+            {
+                this.selectedRow = dataGridView1.Rows[index];
+            }
         }
     }
 }
